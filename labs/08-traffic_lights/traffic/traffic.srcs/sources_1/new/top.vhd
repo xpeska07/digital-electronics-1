61ba016fa3f35b2927dd193d4abeee023da8bc44
student <student@PC-636.urel.feec.vutbr.cz>
2022-04-05 16:25:57 +0200

    update

---
+++ labs/08-traffic_lights/traffic/traffic.srcs/sources_1/new/top.vhd
@@ -0,0 +1,67 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/05/2022 03:56:32 PM
+-- Design Name: 
+-- Module Name: top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity top is
+    Port ( CLK100MHZ : in STD_LOGIC;
+           LED16_R : out STD_LOGIC;
+           LED16_G : out STD_LOGIC;
+           LED16_B : out STD_LOGIC;
+           LED17_R : out STD_LOGIC;
+           LED17_G : out STD_LOGIC;
+           LED17_B : out STD_LOGIC;
+           BTNC : in STD_LOGIC);
+end top;
+
+------------------------------------------------------------
+-- Architecture body for top level
+------------------------------------------------------------
+architecture Behavioral of top is
+
+begin
+    --------------------------------------------------------
+    -- Instance (copy) of tlc entity
+    tlc : entity work.tlc
+        port map(
+            clk   => CLK100MHZ,
+            reset => BTNC,
+            -- MAP TWO RGB LEDS HERE
+            south_o(2)  => LED16_R,
+            south_o(1)  => LED16_G,
+            south_o(0)  => LED16_B,
+            west_o(2)  => LED17_R,
+            west_o(1)  => LED17_G,
+            west_o(0)  => LED17_B
+
+        );
+
+end architecture Behavioral;
